b9a1df7c4382c214cf17d4868b59f959f7363845
David Parker <dav1dp0101@1022606.no-reply.drupal.org>
2018-08-14 10:59:47 -0400

    Salved Node 1.4.3 challenge, creating a Recipes POST router

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ Recipes.create(
   'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);
 Recipes.create(
   'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
+Recipes.create(
+  'scones', ['flour', 'applesauce', 'coconut oil']);
 
 // when the root of this router is called with GET, return
 // all current ShoppingList items
@@ -47,10 +49,24 @@ app.post('/shopping-list', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
-
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 })
+
+app.post('/recipes', jsonParser, (req, res) => {
+  // ensure 'name' and 'ingredients' are in the request body
+  const requiredFields = ['name', 'ingredients'];
+  for (let i=0; i<requiredFields.length; i++) {
+    if (!(requiredFields[i] in req.body)) {
+      const message = `Missing '${requiredFields[i]}' in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = Recipes.create(req.body.name, req.body.ingredients);
+  res.status(201).json(item);
+});
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
